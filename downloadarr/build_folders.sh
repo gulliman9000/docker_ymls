234257a2bdb08c045f1e3ce22a89ba8749b34252
gulliman9000 <nickjacka@gmail.com>
2026-07-07 20:55:23 +1000

    Downloadarr

---
+++ downloadarr/build_folders.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+BASE_DIR="/docker"
+
+COMPOSE_URL="https://raw.githubusercontent.com/gulliman9000/docker_ymls/main/downloadarr/docker_compose.yml"
+
+echo "Creating directory structure..."
+
+mkdir -p "$BASE_DIR"/{sabnzbd,prowlarr,qbittorrent,pinchflat,huntarr,slskd,ytdlp}
+
+echo "Downloading docker-compose.yml..."
+
+curl -fsSL "$COMPOSE_URL" -o "$BASE_DIR/docker-compose.yml"
+
+echo
+echo "Done!"
+echo "Project created at: $BASE_DIR"
