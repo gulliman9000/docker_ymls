90e8b8174725119c97153de148c0a321379309b0
gulliman9000 <nickjacka@gmail.com>
2026-07-07 13:30:54 +1000

    morke

--- servarr/build_folders.sh
+++ servarr/build_folders.sh
@@ -3,12 +3,7 @@ set -e
 BASE_DIR="/docker"
 COMPOSE_URL="https://raw.githubusercontent.com/gulliman9000/docker_ymls/main/servarr/docker_compose.yml"
 echo "Creating directory structure..."
-mkdir -p "$BASE_DIR"/{
-  sonarr,
-  radarr,
-  lidarr,
-  sportarr
-}
+mkdir -p "$BASE_DIR"/{sonarr,radarr,lidarr,sportarr}
 echo "Downloading docker-compose.yml..."
 curl -fsSL "$COMPOSE_URL" -o "$BASE_DIR/docker-compose.yml"
 echo
