f0a94f3cc0890dd710c5b273b86c4f8aef881178
gulliman9000 <nickjacka@gmail.com>
2026-07-07 22:19:07 +1000

    more soul

--- downloadarr/build_folders.sh
+++ downloadarr/build_folders.sh
@@ -5,6 +5,7 @@ set -e
 BASE_DIR="/docker"
 
 COMPOSE_URL="https://raw.githubusercontent.com/gulliman9000/docker_ymls/main/downloadarr/docker_compose.yml"
+SOULARR_CONFIG="https://raw.githubusercontent.com/gulliman9000/docker_ymls/main/downloadarr/soularr_config.ini"
 
 echo "Creating directory structure..."
 
@@ -13,6 +14,7 @@ mkdir -p "$BASE_DIR"/{sabnzbd,prowlarr,qbittorrent,pinchflat,soularr,slskd,ytdlp
 echo "Downloading docker-compose.yml..."
 
 curl -fsSL "$COMPOSE_URL" -o "$BASE_DIR/docker-compose.yml"
+curl -fsSL "$SOULARR_CONFIG" -o "$BASE_DIR/soularrd/config.ini"
 
 echo
 echo "Done!"
